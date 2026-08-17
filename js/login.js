c6a89483c30b248883759de8d7cfb98aac3b5420
mysoyul <vega2k_@naver.com>
2026-08-17 19:27:58 +0900

    login.js add

---
+++ js/login.js
@@ -0,0 +1,14 @@
+//index.html 을 위한 js file
+function login(username, password) {
+    if(username){
+        console.log(username);
+    }else if(username && password) {
+        console.log(username + ' ' + password);
+    }else {
+        console.log('>>>> login fail');
+    }
+};
+
+function logout() {
+    console.log('로그아웃');
+}
